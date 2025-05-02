6cccc38be7b958a7a57a050ecd4e7c4064daf6f0
Francisca <francisca.paredes.pr@gmail.com>
2025-05-02 15:53:04 -0400

    model user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    # Enlace al usuario estándar de Django
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Datos nutricionales y físicos
+    birth_date = models.DateField(null=True, blank=True)
+    sex = models.CharField(max_length=10, choices=[('male', 'Hombre'), ('female', 'Mujer'), ('other', 'Otro')])
+    height_cm = models.PositiveIntegerField(null=True, blank=True)  # altura en cm
+    weight_kg = models.FloatField(null=True, blank=True)
+    tmb = models.FloatField(null=True, blank=True, help_text="Tasa metabólica basal")
+
+    # Rol futuro
+    ROLE_CHOICES = [
+        ('user', 'Usuario'),
+        ('nutritionist', 'Nutricionista'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+
+
+
+class Account(AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.CharField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=50)
+
+
+    # Campos por defecto django
+    date_joined = models.DateTimeField(auto_now_add=True)
+    date_login = models.DateField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
+from .models import UserProfile
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # inicia sesión automáticamente
+            return redirect('profile')  # redirige al perfil
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+@login_required
+def profile_view(request):
+    profile = request.user.userprofile
+    return render(request, 'accounts/profile.html', {'profile': profile})
