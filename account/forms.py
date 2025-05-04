186076cb5e922d5ed2d678f09f6aaa8c26181a49
Francisca <francisca.paredes.pr@gmail.com>
2025-05-04 12:11:02 -0400

    form register

--- account/forms.py
+++ account/forms.py
@@ -29,10 +29,14 @@ class RegistrationForm(forms.ModelForm):
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
 
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        confirm_password = cleaned_data.get("confirm_password")
 
-        if password and confirm_password and password != confirm_password:
-            self.add_error('confirm_password', "Las contraseñas no coinciden.")
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                'El password no coincide!'
+            )
+        
